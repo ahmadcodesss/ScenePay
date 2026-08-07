57eb4b78e60a9e4ba17f1b3ba9e523d70d2d41a5
Ahmadcodesss <ahmadiftikhar6789@gmail.com>
2026-08-08 00:35:41 +0500

    commented out few lines in parse.py to get app running locally

--- utils/parse.py
+++ utils/parse.py
@@ -32,10 +32,10 @@ def parse_bill_text(text):
                   items[dish] = chosen_price
       
       return items
-load_dotenv()  # at top of your app
-pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_CMD", "tesseract")
-text=pytesseract.image_to_string(r"C:\Users\DELL\Downloads\q.jpeg")
-dishes=parse_bill_text(text)
+# load_dotenv()  # at top of your app
+# pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_CMD", "tesseract")
+# text=pytesseract.image_to_string(r"C:\Users\DELL\Downloads\q.jpeg")
+# dishes=parse_bill_text(text)
 
 # print("\n"+text+"\n")
 # for dish,price in dishes.items():
